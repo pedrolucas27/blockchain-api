d6c52d533166958e11fd1f16e1db04264dd54a74
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-01-16 21:36:15 -0300

    feat: funções generate_hash e get_block_id

--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -1,4 +1,6 @@
 use serde::Serialize;
+use serde_json::{self, Value};
+use sha2::{Digest, Sha256};
 use std::collections::HashSet;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -28,7 +30,7 @@ impl Blockchain {
         let merkle_root = self.generate_merkle_root(&self.mempool);
 
         let previous_hash = if let Some(last_block) = self.chain.last() {
-            self.get_block_id(last_block)
+            Blockchain::get_block_id(last_block)
         } else {
             "0".repeat(64)
         };
@@ -56,7 +58,20 @@ impl Blockchain {
         transactions.join("-")
     }
 
-    fn get_block_id(&self, block: &Block) -> String {
-        format!("{}-{}-{}", block.index, block.timestamp, block.merkle_root)
+    pub fn get_block_id(block: &Block) -> String {
+        let mut block_copy =
+            serde_json::to_value(block).expect("Erro ao converter bloco para JSON");
+        if let Value::Object(ref mut map) = block_copy {
+            map.remove("transactions");
+        }
+        let serialized =
+            serde_json::to_string(&block_copy).expect("Erro ao serializar o bloco modificado");
+        Self::generate_hash(&serialized)
+    }
+
+    pub fn generate_hash(data: &str) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(data.as_bytes());
+        format!("{:x}", hasher.finalize())
     }
 }
