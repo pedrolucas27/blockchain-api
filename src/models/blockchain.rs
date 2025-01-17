368bef4e681186e057fcd1e178696a13d42c41d7
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-01-16 22:10:41 -0300

    feat: funções is_valid_proof e mine_proof_of_work

--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -20,6 +20,8 @@ pub struct Blockchain {
     pub nodes: HashSet<String>,
 }
 
+const DIFFICULTY: usize = 4;
+
 impl Blockchain {
     pub fn create_block(&mut self) -> Block {
         let timestamp = SystemTime::now()
@@ -30,7 +32,7 @@ impl Blockchain {
         let merkle_root = self.generate_merkle_root(&self.mempool);
 
         let previous_hash = if let Some(last_block) = self.chain.last() {
-            Blockchain::get_block_id(last_block)
+            Self::get_block_id(last_block)
         } else {
             "0".repeat(64)
         };
@@ -73,5 +75,22 @@ impl Blockchain {
         let mut hasher = Sha256::new();
         hasher.update(data.as_bytes());
         format!("{:x}", hasher.finalize())
+    }
+
+    pub fn is_valid_proof(block: &mut Block, nonce: u64) -> bool {
+        block.nonce = nonce;
+        let block_id = Blockchain::generate_hash(
+            &serde_json::to_string(block).expect("Erro ao serializar o bloco"),
+        );
+        block_id.chars().take(DIFFICULTY).all(|c| c == '0')
+    }
+
+    pub fn mine_proof_of_work(&self, block: &mut Block) -> u64{
+        // Retorna um nonce válido para o bloco passado
+        let mut nonce: u64 = 0;
+        while Self::is_valid_proof(block, nonce) == false {
+            nonce += 1;
+        }
+        nonce
     }
 }
