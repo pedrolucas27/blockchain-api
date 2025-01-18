5da2b292b680f2c5575082610a62865f1e126aeb
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-01-17 23:52:54 -0300

    feat: adiciona struct de transaction e métodos de criar transação, já implementando a assinatura das transações

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,39 @@ fn main() {
     //     .bind("localhost:8080")?
     //     .run()
     //     .await
-    let mut blockchain = Blockchain {
-        chain: vec![],
-        mempool: vec!["tx1".to_string(), "tx2".to_string()], 
-    };
+    let mut blockchain = Blockchain::new();
+
+    println!("Block genesis: {:#?}", blockchain.chain.first());
+
     let new_block = blockchain.create_block();
     println!("Novo bloco criado: {:#?}", new_block);
 
     let hash = Blockchain::generate_hash("dados para hash");
     println!("Hash gerada: {}", hash);
-    
+
     let block_hash = Blockchain::get_block_id(&new_block);
     println!("Hash do bloco: {}", block_hash);
+
+    let wif_key = "L1US57sChKZeyXrev9q7tFm2dgA2ktJe2NP3xzXRv6wizom5MN1U";
+    let message = "Mensagem para assinar";
+
+    match Blockchain::sign(wif_key, message) {
+        Ok(signature) => println!("Assinatura gerada: {:?}", signature),
+        Err(e) => println!("Erro ao assinar: {}", e),
+    }
+
+    let transaction = blockchain.create_transaction(
+        "19sXoSbfcQD9K66f5hwP5vLwsaRyKLPgXF",
+        "1MxTkeEP2PmHSMze5tUZ1hAV3YTKu2Gh1N",
+        100,
+        "123123123",
+        "L1US57sChKZeyXrev9q7tFm2dgA2ktJe2NP3xzXRv6wizom5MN1U",
+    );
+
+    println!("transação: {:#?}", transaction);
+
+    let mut new_block = blockchain.create_block();
+    Blockchain::mine_proof_of_work(&mut new_block);
+    println!("Novo bloco criado de novo: {:#?}", new_block);
+
 }
--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -1,13 +1,16 @@
+use bitcoin::hashes::Hash;
+use bitcoin::key::PrivateKey;
 use serde::Serialize;
 use serde_json::{self, Value};
 use sha2::{Digest, Sha256};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH}; 
+use bitcoin::secp256k1::{ecdsa::Signature, Message, Secp256k1};
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Block {
     pub index: usize,
     pub timestamp: u64,
-    pub transactions: Vec<String>,
+    pub transactions: Vec<Transaction>,
     pub merkle_root: String,
     pub nonce: u64,
     pub previous_hash: String,
@@ -15,7 +18,17 @@ pub struct Block {
 
 pub struct Blockchain {
     pub chain: Vec<Block>,
-    pub mempool: Vec<String>,
+    pub mempool: Vec<Transaction>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Transaction {
+    sender: String,
+    recipient: String,
+    amount: u64,
+    timestamp: String,
+    signature: Option<String>, // Por causa de problemas com o derive Serialize no Signature
+                               // signature: Option<Result<Signature, std::string::String>>,
 }
 
 const DIFFICULTY: usize = 4;
@@ -37,7 +50,7 @@ impl Blockchain {
         // Usado apenas no construtor. Cria, minera e retorna o bloco gênesis do blockchain
         let genesis_block = self.create_block();
 
-        if let Some(last_block) = self.chain.last_mut() { 
+        if let Some(last_block) = self.chain.last_mut() {
             Self::mine_proof_of_work(last_block);
         } else {
             println!("A chain está vazia!");
@@ -47,6 +60,7 @@ impl Blockchain {
     }
 
     pub fn create_block(&mut self) -> Block {
+        // Cria um novo bloco, inclui todas as transações pendentes e adiciona ao chain. O bloco ainda não tem nonce válido, deve ser minerado
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Erro ao obter o tempo")
@@ -75,12 +89,41 @@ impl Blockchain {
         block
     }
 
-    fn generate_merkle_root(&self, transactions: &[String]) -> String {
+    fn generate_merkle_root(&self, transactions: &Vec<Transaction>) -> String {
+        // Retorna a Merkle Root de um conjunto de transações
         if transactions.is_empty() {
             return "0".repeat(64);
         }
 
-        transactions.join("-")
+        let mut tx_hashes: Vec<String> = transactions
+            .iter()
+            .map(|tx| {
+                let serialized =
+                    serde_json::to_string(&tx).expect("Erro ao serializar a transação modificado");
+                Self::generate_hash(&serialized)
+            })
+            .collect();
+
+        Self::hash_tx_hashes(&mut tx_hashes)
+    }
+
+    fn hash_tx_hashes(tx_hashes: &mut Vec<String>) -> String {
+        // Função auxiliar recursiva para cálculo da Merkle Root
+        if tx_hashes.len() == 1 {
+            return tx_hashes[0].clone();
+        }
+
+        if tx_hashes.len() % 2 != 0 {
+            tx_hashes.push(tx_hashes.last().unwrap().clone());
+        }
+
+        let mut new_tx_hashes = vec![];
+        for i in (0..tx_hashes.len()).step_by(2) {
+            let concatenated = format!("{}{}", tx_hashes[i], tx_hashes[i + 1]);
+            new_tx_hashes.push(Blockchain::generate_hash(&concatenated));
+        }
+
+        Self::hash_tx_hashes(&mut new_tx_hashes)
     }
 
     pub fn get_block_id(block: &Block) -> String {
@@ -96,12 +139,14 @@ impl Blockchain {
     }
 
     pub fn generate_hash(data: &str) -> String {
+        // Gera um hash a partir dos dados passados como argumento
         let mut hasher = Sha256::new();
         hasher.update(data.as_bytes());
         format!("{:x}", hasher.finalize())
     }
 
     pub fn is_valid_proof(block: &mut Block, nonce: u64) -> bool {
+        // Verifica se o nonce é válido para o bloco. Um nonce válido é um número que, gera um hash com 'DIFFICULTY' zeros à esquerda
         block.nonce = nonce;
         let block_id = Blockchain::generate_hash(
             &serde_json::to_string(block).expect("Erro ao serializar o bloco"),
@@ -116,5 +161,61 @@ impl Blockchain {
             nonce += 1;
         }
         nonce
+    }
+
+    pub fn create_transaction(
+        &mut self,
+        sender: &str,
+        recipient: &str,
+        amount: u64,
+        timestamp: &str,
+        priv_wif_key: &str,
+    ) -> Transaction {
+        // Cria, insere no mempool e retorna uma nova transação, assinada pela chave privada WIF do remetente.
+
+        let mut transaction = Transaction {
+            sender: sender.to_string(),
+            recipient: recipient.to_string(),
+            amount,
+            timestamp: timestamp.to_string(),
+            signature: None,
+        };
+
+        // A message enviada é o cabeçalho da transação, sem o signature
+        let tx_data =
+            serde_json::to_string(&transaction).expect("Erro ao serializar a transação para JSON");
+
+        if let Ok(signature) = Blockchain::sign(priv_wif_key, &tx_data) {
+            // Converte a assinatura para uma string hexadecimal, por causa do derive serialize
+            transaction.signature = Some(signature.to_string());
+        }
+
+        // transaction.signature = Some(Blockchain::sign(priv_wif_key, "message"));
+
+        self.mempool.push(transaction.clone());
+
+        transaction
+    }
+
+    pub fn sign(wif_compressed_priv_key: &str, message: &str) -> Result<Signature, String> {
+        // Retorna a assinatura digital da mensagem e a respectiva chave privada WIF-compressed
+        let secp = Secp256k1::new();
+
+        // Converte a chave privada WIF-compressed para `SecretKey`
+        let private_key = PrivateKey::from_wif(wif_compressed_priv_key)
+            .map_err(|e| format!("Erro ao interpretar WIF: {}", e))?;
+
+        let secret_key = private_key.inner;
+
+        // Gera a mensagem como um hash de 32 bytes (SHA256)
+        let msg_hash = bitcoin::hashes::sha256::Hash::hash(message.as_bytes());
+
+        let message = Message::from_digest_slice(msg_hash.as_ref())
+            .map_err(|e| format!("Erro ao criar a mensagem: {}", e))?;
+
+        // Assina a mensagem
+        let signature = secp.sign_ecdsa(&message, &secret_key);
+
+        Ok(signature)
     }
 }
